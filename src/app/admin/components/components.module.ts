7c082a1f29711108049108144a3f979c02f953cd
tarhan84 <tarhan.werek@gmail.com>
2023-09-22 19:42:24 +0300

    add ua module and components

--- src/app/admin/components/components.module.ts
+++ src/app/admin/components/components.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductModule } from './products/product.module';
+import { CustomerModule } from './customers/customer.module';
+import { OrderModule } from './orders/order.module';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductModule,
+    CustomerModule,
+    OrderModule,
+    DashboardModule
   ]
 })
 export class ComponentsModule { }
