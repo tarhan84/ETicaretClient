97376cec1b5b509074d2ea03114223753d932171
tarhan84 <tarhan.werek@gmail.com>
2023-09-22 17:01:39 +0300

    create layout module and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AdminModule } from './admin/admin.module';
+import { UiModule } from './ui/ui.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule,
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
